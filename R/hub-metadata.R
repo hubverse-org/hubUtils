cadb44005693234fd6407ee37604d6e11ad9ed9e
annakrystalli <annakrystalli@users.noreply.github.com>
2022-10-05 08:15:04 +0000

    Style code

--- R/hub-metadata.R
+++ R/hub-metadata.R
@@ -1,46 +1,46 @@
 get_task_id_params <- function(x,
                                param = c(
-                                   "location",
-                                   "horizon",
-                                   "origin_date"
+                                 "location",
+                                 "horizon",
+                                 "origin_date"
                                ),
                                flatten = FALSE) {
-    param <- match.arg(param)
-    values <- x$model_tasks[[1]]$task_ids[[param]]
+  param <- match.arg(param)
+  values <- x$model_tasks[[1]]$task_ids[[param]]
 
-    if (flatten) {
-        out <- c(values$required,
-                 values$optional) |>
-            unlist() |>
-            unname()
-    } else {
-        out <- list(
-            required = purrr::pluck(
-                values,
-                "required"
-            ) |>
-                unlist(),
-            optional = purrr::pluck(
-                values,
-                "optional"
-            ) |>
-                unlist()
-        )
-    }
-
-    out <- structure(out,
-                     units = purrr::pluck(
-                         values,
-                         "units"
-                     ) |>
-                         unlist(),
-                     format = purrr::pluck(
-                         values,
-                         "format"
-                     ) |>
-                         unlist()
+  if (flatten) {
+    out <- c(
+      values$required,
+      values$optional
+    ) |>
+      unlist() |>
+      unname()
+  } else {
+    out <- list(
+      required = purrr::pluck(
+        values,
+        "required"
+      ) |>
+        unlist(),
+      optional = purrr::pluck(
+        values,
+        "optional"
+      ) |>
+        unlist()
     )
-    return(out)
+  }
+
+  out <- structure(out,
+    units = purrr::pluck(
+      values,
+      "units"
+    ) |>
+      unlist(),
+    format = purrr::pluck(
+      values,
+      "format"
+    ) |>
+      unlist()
+  )
+  return(out)
 }
-
-
