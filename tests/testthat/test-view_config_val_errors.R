2edcad5bb76f8c4ed622d64bc99d1eb41c9338ff
Anna Krystalli <annakrystalli@googlemail.com>
2023-02-22 13:07:44 +0200

    add view_config_val_errors tests

--- tests/testthat/test-view_config_val_errors.R
+++ tests/testthat/test-view_config_val_errors.R
@@ -9,3 +9,40 @@ test_that("Errors report launch successful", {
         gt:::render_as_html()
     expect_snapshot(tbl)
 })
+
+
+test_that("Data column handled correctly when required property missing", {
+    set.seed(1)
+    # One nested property missing, one type error
+    config_path <- testthat::test_path("testdata", "tasks_required_missing.json")
+    tbl <- view_config_val_errors(suppressWarnings(
+        validate_config(config_path = config_path)
+    )) %>%
+        gt::as_raw_html()
+    expect_snapshot(tbl)
+
+    skip_on_ci()
+    # Only a single property missing
+    config_path <- testthat::test_path("testdata", "tasks_required_missing_only.json")
+    tbl <- view_config_val_errors(suppressWarnings(
+        validate_config(config_path = config_path)
+    )) %>%
+        gt:::render_as_html()
+    expect_snapshot(tbl)
+
+    # Two properties missing, only one nested
+    config_path <- testthat::test_path("testdata", "tasks_required_missing_only2.json")
+    tbl <- view_config_val_errors(suppressWarnings(
+        validate_config(config_path = config_path)
+    )) %>%
+        gt:::render_as_html()
+    expect_snapshot(tbl)
+
+    # Two properties missing, both nested
+    config_path <- testthat::test_path("testdata", "tasks_required_missing_only2b.json")
+    tbl <- view_config_val_errors(suppressWarnings(
+        validate_config(config_path = config_path)
+    )) %>%
+        gt:::render_as_html()
+    expect_snapshot(tbl)
+})
