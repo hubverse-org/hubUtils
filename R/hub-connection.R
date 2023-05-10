d77aca26dfb438548d3969cbe307fa56586ce257
Anna Krystalli <annakrystalli@googlemail.com>
2023-05-10 13:58:00 +0300

    move print methods to separate file

--- R/hub-connection.R
+++ R/hub-connection.R
@@ -101,8 +101,10 @@ connect_hub.default <- function(hub_path,
   )
 
   if (inherits(dataset, "UnionDataset")) {
-    file_system <- purrr::map_chr(dataset$children,
-                   ~class(.x$filesystem)[1]) %>%
+    file_system <- purrr::map_chr(
+      dataset$children,
+      ~ class(.x$filesystem)[1]
+    ) %>%
       unique()
   } else {
     file_system <- class(dataset$filesystem)[1]
@@ -209,8 +211,10 @@ connect_hub.SubTreeFileSystem <- function(hub_path,
   )
 
   if (inherits(dataset, "UnionDataset")) {
-    file_system <- purrr::map_chr(dataset$children,
-                                  ~class(.x$filesystem$base_fs)[1]) %>%
+    file_system <- purrr::map_chr(
+      dataset$children,
+      ~ class(.x$filesystem$base_fs)[1]
+    ) %>%
       unique()
   } else {
     file_system <- class(dataset$filesystem$base_fs)[1]
@@ -284,109 +288,6 @@ connect_model_output.SubTreeFileSystem <- function(model_output_dir,
   )
 }
 
-#' Print a `<hub_connection>` or `<mod_out_connection>` S3 class object
-#'
-#' @param x A `<hub_connection>` or `<mod_out_connection>` S3 class object.
-#'
-#' @param verbose Logical. Whether to print the full structure of the object.
-#' Defaults to `FALSE`.
-#' @param ... Further arguments passed to or from other methods.
-#'
-#' @export
-#' @describeIn print.hub_connection print a `<hub_connection>` object.
-#' @examples
-#' hub_path <- system.file("testhubs/simple", package = "hubUtils")
-#' hub_con <- connect_hub(hub_path)
-#' hub_con
-#' print(hub_con)
-#' print(hub_con, verbose = TRUE)
-#' mod_out_path <- system.file("testhubs/simple/model-output", package = "hubUtils")
-#' mod_out_con <- connect_model_output(mod_out_path)
-#' print(mod_out_con)
-print.hub_connection <- function(x, verbose = FALSE, ...) {
-  cli::cli_h2("{.cls {class(x)[1:2]}}")
-
-  print_msg <- NULL
-
-
-  if (!is.null(attr(x, "hub_path"))) {
-    print_msg <- c(print_msg,
-      "*" = "hub_name: {.val {attr(x, 'hub_name')}}",
-      "*" = "hub_path: {.file {attr(x, 'hub_path')}}"
-    )
-  }
-  if (!is.null(attr(x, "file_format"))) {
-    print_msg <- c(print_msg,
-      "*" = "file_format: {.val {attr(x, 'file_format')}}"
-    )
-  }
-  if (!is.null(attr(x, "file_system"))) {
-    print_msg <- c(print_msg,
-      "*" = "file_system: {.val {attr(x, 'file_system')[1]}}"
-    )
-  }
-  print_msg <- c(print_msg,
-    "*" = "model_output_dir: {.val {attr(x, 'model_output_dir')}}"
-  )
-
-  if (!is.null(attr(x, "config_admin"))) {
-    print_msg <- c(print_msg,
-      "*" = "config_admin: {.path hub-config/admin.json}"
-    )
-  }
-  if (!is.null(attr(x, "config_tasks"))) {
-    print_msg <- c(print_msg,
-      "*" = "config_tasks: {.path hub-config/tasks.json}"
-    )
-  }
-
-  cli::cli_bullets(print_msg)
-
-  if (inherits(x, "ArrowObject")) {
-    cli::cli_h3("Connection schema")
-    x$print()
-  }
-
-  if (verbose) {
-    utils::str(x)
-  }
-  invisible(x)
-}
-
-#' @export
-#' @describeIn print.hub_connection print a `<mod_out_connection>` object.
-print.mod_out_connection <- function(x, verbose = FALSE, ...) {
-  cli::cli_h2("{.cls {class(x)[1:2]}}")
-
-  print_msg <- NULL
-
-  if (!is.null(attr(x, "file_format"))) {
-    print_msg <- c(print_msg,
-      "*" = "file_format: {.val {attr(x, 'file_format')}}"
-    )
-  }
-  if (!is.null(attr(x, "file_system"))) {
-    print_msg <- c(print_msg,
-      "*" = "file_system: {.val {attr(x, 'file_system')}}"
-    )
-  }
-  print_msg <- c(print_msg,
-    "*" = "model_output_dir: {.val {attr(x, 'model_output_dir')}}"
-  )
-
-  cli::cli_bullets(print_msg)
-
-  if (inherits(x, "ArrowObject")) {
-    cli::cli_h3("Connection schema")
-    x$print()
-  }
-
-  if (verbose) {
-    utils::str(x)
-  }
-  invisible(x)
-}
-
 get_file_format <- function(config_admin,
                             file_format = c("csv", "parquet", "arrow"),
                             call = rlang::caller_env()) {
@@ -420,7 +321,6 @@ get_file_format <- function(config_admin,
   }
 
   return(config_file_format)
-
 }
 
 model_output_dir_path <- function(hub_path, config_admin,
---
+++ R/print.R
@@ -0,0 +1,102 @@
+#' Print a `<hub_connection>` or `<mod_out_connection>` S3 class object
+#'
+#' @param x A `<hub_connection>` or `<mod_out_connection>` S3 class object.
+#'
+#' @param verbose Logical. Whether to print the full structure of the object.
+#' Defaults to `FALSE`.
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @export
+#' @describeIn print.hub_connection print a `<hub_connection>` object.
+#' @examples
+#' hub_path <- system.file("testhubs/simple", package = "hubUtils")
+#' hub_con <- connect_hub(hub_path)
+#' hub_con
+#' print(hub_con)
+#' print(hub_con, verbose = TRUE)
+#' mod_out_path <- system.file("testhubs/simple/model-output", package = "hubUtils")
+#' mod_out_con <- connect_model_output(mod_out_path)
+#' print(mod_out_con)
+print.hub_connection <- function(x, verbose = FALSE, ...) {
+    cli::cli_h2("{.cls {class(x)[1:2]}}")
+
+    print_msg <- NULL
+
+
+    if (!is.null(attr(x, "hub_path"))) {
+        print_msg <- c(print_msg,
+                       "*" = "hub_name: {.val {attr(x, 'hub_name')}}",
+                       "*" = "hub_path: {.file {attr(x, 'hub_path')}}"
+        )
+    }
+    if (!is.null(attr(x, "file_format"))) {
+        print_msg <- c(print_msg,
+                       "*" = "file_format: {.val {attr(x, 'file_format')}}"
+        )
+    }
+    if (!is.null(attr(x, "file_system"))) {
+        print_msg <- c(print_msg,
+                       "*" = "file_system: {.val {attr(x, 'file_system')[1]}}"
+        )
+    }
+    print_msg <- c(print_msg,
+                   "*" = "model_output_dir: {.val {attr(x, 'model_output_dir')}}"
+    )
+
+    if (!is.null(attr(x, "config_admin"))) {
+        print_msg <- c(print_msg,
+                       "*" = "config_admin: {.path hub-config/admin.json}"
+        )
+    }
+    if (!is.null(attr(x, "config_tasks"))) {
+        print_msg <- c(print_msg,
+                       "*" = "config_tasks: {.path hub-config/tasks.json}"
+        )
+    }
+
+    cli::cli_bullets(print_msg)
+
+    if (inherits(x, "ArrowObject")) {
+        cli::cli_h3("Connection schema")
+        x$print()
+    }
+
+    if (verbose) {
+        utils::str(x)
+    }
+    invisible(x)
+}
+
+#' @export
+#' @describeIn print.hub_connection print a `<mod_out_connection>` object.
+print.mod_out_connection <- function(x, verbose = FALSE, ...) {
+    cli::cli_h2("{.cls {class(x)[1:2]}}")
+
+    print_msg <- NULL
+
+    if (!is.null(attr(x, "file_format"))) {
+        print_msg <- c(print_msg,
+                       "*" = "file_format: {.val {attr(x, 'file_format')}}"
+        )
+    }
+    if (!is.null(attr(x, "file_system"))) {
+        print_msg <- c(print_msg,
+                       "*" = "file_system: {.val {attr(x, 'file_system')}}"
+        )
+    }
+    print_msg <- c(print_msg,
+                   "*" = "model_output_dir: {.val {attr(x, 'model_output_dir')}}"
+    )
+
+    cli::cli_bullets(print_msg)
+
+    if (inherits(x, "ArrowObject")) {
+        cli::cli_h3("Connection schema")
+        x$print()
+    }
+
+    if (verbose) {
+        utils::str(x)
+    }
+    invisible(x)
+}
