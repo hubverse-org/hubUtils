25e72e03a3d050ab96cba0db5569bfa793ff4991
Anna Krystalli <annakrystalli@googlemail.com>
2023-06-12 16:39:55 -0500

    Add create_hub_schema tests

---
+++ tests/testthat/test-create_hub_schema.R
@@ -0,0 +1,24 @@
+test_that("create_hub_schema works correctly", {
+     hub_path <- system.file("testhubs/simple", package = "hubUtils")
+     config_tasks <- read_config(hub_path, "tasks")
+
+     schema_csv <- create_hub_schema(config_tasks, format = "csv")
+     expect_equal(
+         schema_csv$ToString(),
+         "origin_date: date32[day]\ntarget: string\nhorizon: int32\nlocation: string\nage_group: string\noutput_type: string\noutput_type_id: double\nvalue: int32"
+     )
+
+     schema_csv <- create_hub_schema(config_tasks, format = "csv",
+                                     output_type_id_datatype = "character")
+     expect_equal(
+         schema_csv$ToString(),
+         "origin_date: date32[day]\ntarget: string\nhorizon: int32\nlocation: string\nage_group: string\noutput_type: string\noutput_type_id: string\nvalue: int32"
+     )
+
+
+     schema_parquet <- create_hub_schema(config_tasks, format = "parquet")
+     expect_equal(
+         schema_parquet$ToString(),
+         "origin_date: date32[day]\ntarget: string\nhorizon: int32\nlocation: string\nage_group: string\noutput_type: string\noutput_type_id: double\nvalue: int32\nmodel_id: string"
+     )
+})
