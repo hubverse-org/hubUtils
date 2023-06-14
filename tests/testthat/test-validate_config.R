02b9bf8833a4761cb14faa7dcb1c5ae58f7aed22
Anna Krystalli <annakrystalli@googlemail.com>
2023-06-14 16:42:57 -0500

    Validate that task ids don't conflict with standard column names. Resolves #67

--- tests/testthat/test-validate_config.R
+++ tests/testthat/test-validate_config.R
@@ -69,6 +69,12 @@ test_that("Dynamic config errors detected successfully by custom R validation", 
   expect_false(out)
 })
 
+test_that("Reserved hub variable task id name detected correctly", {
+  config_path <- testthat::test_path("testdata", "tasks-errors-rval-reserved.json")
+  out <- suppressWarnings(validate_config(config_path = config_path))
+  expect_snapshot(out)
+  expect_false(out)
+})
 
 test_that("NULL target keys validated successfully", {
   config_path <- testthat::test_path("testdata", "tasks_null_rval.json")
