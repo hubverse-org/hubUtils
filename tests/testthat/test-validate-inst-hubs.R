452f33e67a09d415ce5c90dcd84f1593971cd10d
Anna Krystalli <annakrystalli@googlemail.com>
2023-07-18 11:23:39 +0300

    Add inst test hub config validation tests

---
+++ tests/testthat/test-validate-inst-hubs.R
@@ -0,0 +1,31 @@
+test_that("simple example hub configured correctly", {
+  expect_true(
+    all(
+      unlist(
+        suppressMessages(
+          validate_hub_config(
+            hub_path = system.file("testhubs/simple",
+              package = "hubUtils"
+            )
+          )
+        )
+      )
+    )
+  )
+})
+
+test_that("flusight example hub configured correctly", {
+    expect_true(
+        all(
+            unlist(
+                suppressMessages(
+                    validate_hub_config(
+                        hub_path = system.file("testhubs/flusight",
+                                               package = "hubUtils"
+                        )
+                    )
+                )
+            )
+        )
+    )
+})
