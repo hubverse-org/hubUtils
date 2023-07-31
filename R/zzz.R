9452b1583d1d3802ddc064b7cd61f2f8296dd018
Anna Krystalli <annakrystalli@googlemail.com>
2023-07-31 12:34:05 +0300

    Memoise read_config. Resolves #101

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   get_schema <<- memoise::memoise(get_schema)
   get_schema_valid_versions <<- memoise::memoise(get_schema_valid_versions)
+  read_config <<- memoise::memoise(read_config)
 }
