2a24b86c56b8ef6aaaf8fe82ea825876601ec88a
Anna Krystalli <annakrystalli@googlemail.com>
2024-03-06 11:32:28 +0200

    style

--- tests/testthat/test-utils-round-config.R
+++ tests/testthat/test-utils-round-config.R
@@ -30,32 +30,32 @@ test_that("get_round_task_id_names fails correctly", {
 })
 
 test_that("get_round_model_tasks works", {
-    hub_path <- system.file("testhubs/simple", package = "hubUtils")
-    config_tasks <- read_config(hub_path)
+  hub_path <- system.file("testhubs/simple", package = "hubUtils")
+  config_tasks <- read_config(hub_path)
 
-    expect_snapshot(
-        get_round_model_tasks(config_tasks, round_id = "2022-10-01")
-    )
-    expect_snapshot(
-        get_round_model_tasks(config_tasks, round_id = "2022-10-22")
-    )
+  expect_snapshot(
+    get_round_model_tasks(config_tasks, round_id = "2022-10-01")
+  )
+  expect_snapshot(
+    get_round_model_tasks(config_tasks, round_id = "2022-10-22")
+  )
 })
 
 test_that("get_round_model_tasks fails correctly", {
-    hub_path <- system.file("testhubs/simple", package = "hubUtils")
-    config_tasks <- read_config(hub_path)
+  hub_path <- system.file("testhubs/simple", package = "hubUtils")
+  config_tasks <- read_config(hub_path)
 
-    expect_snapshot(
-        get_round_model_tasks(
-            config_tasks = c("random", "character", "vector"),
-            round_id = "2022-10-01"
-        ),
-        error = TRUE
-    )
-    expect_snapshot(
-        get_round_model_tasks(config_tasks,
-                                round_id = c("2022-10-01", "2022-10-22")
-        ),
-        error = TRUE
-    )
+  expect_snapshot(
+    get_round_model_tasks(
+      config_tasks = c("random", "character", "vector"),
+      round_id = "2022-10-01"
+    ),
+    error = TRUE
+  )
+  expect_snapshot(
+    get_round_model_tasks(config_tasks,
+      round_id = c("2022-10-01", "2022-10-22")
+    ),
+    error = TRUE
+  )
 })
