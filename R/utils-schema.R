24a4130efa1490ed1ece8ab5a708a4470b102c1b
Anna Krystalli <annakrystalli@googlemail.com>
2024-05-15 18:34:54 +0300

    Move extract_schema_version from hubAdmin to hubUtils

--- R/utils-schema.R
+++ R/utils-schema.R
@@ -132,3 +132,14 @@ validate_schema_version <- function(schema_version, branch) {
     )
   }
 }
+
+#' Extract the schema version from a schema `id` or config `schema_version` property
+#' character string
+#'
+#' @param id A schema `id` or config `schema_version` property character string.
+#'
+#' @return The schema version number as a character string.
+#' @export
+extract_schema_version <- function(id) {
+  stringr::str_extract(id, "v([0-9]\\.){2}[0-9](\\.[0-9]+)?")
+}
--- tests/testthat/test-utils-schema.R
+++ tests/testthat/test-utils-schema.R
@@ -44,3 +44,11 @@ test_that("validate_schema_version works", {
     error = TRUE
   )
 })
+
+test_that("extract_schema_version works", {
+  expect_equal(
+    extract_schema_version(
+      "https://raw.githubusercontent.com/Infectious-Disease-Modeling-Hubs/schemas/main/v3.0.0/tasks-schema.json"
+    ), "v3.0.0"
+  )
+})
