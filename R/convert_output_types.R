2a6c5955fce2f9396e6a19f4352c3f6a46683aa5
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2024-06-06 15:46:39 -0400

    use get_task_id_cols()

--- R/convert_output_types.R
+++ R/convert_output_types.R
@@ -4,9 +4,6 @@
 #' @param model_outputs an object of class `model_out_tbl` with component model
 #'    outputs (e.g., predictions). `model_outputs` should contain only one
 #'    unique value in the `output_type` column
-#' @param group_by_cols a `vector` of task_id column names, i.e.,
-#'    specify which columns to group by when transforming; the `model_id`
-#'    column will be included automatically
 #' @param new_output_type `string` indicating the desired output_type after
 #'   transformation; can be `"mean"`, `"median"`, `"quantile"`, `"cdf"`; see
 #'   details for supported conversions
@@ -51,19 +48,19 @@
 #'
 #' @return object of class `model_out_tbl` containing new output_type
 #' @export
-convert_output_type <- function(model_outputs, group_by_cols,
-                                new_output_type, new_output_type_id = NA,
-                                n_samples = 1e4, ...) {
-  # for cdf and quantile functions, get samples
+convert_output_type <- function(model_outputs, new_output_type,
+                                new_output_type_id = NA, n_samples = 1e4, ...) {
+  # validations
+  task_id_cols <- get_task_id_cols(model_outputs)
   starting_output_type <- model_outputs$output_type %>% unique()
   starting_output_type_ids <- model_outputs$output_type_id %>% unique()
-  validate_new_output_type(
-    starting_output_type, new_output_type,
-    new_output_type_id
-  )
+  task_id_cols <- get_task_id_cols(model_outputs)
+  validate_new_output_type(starting_output_type, new_output_type,
+                           new_output_type_id)
+  # for cdf and quantile functions, get samples
   if (starting_output_type == "cdf") {
     # estimate from samples
-    model_outputs <- get_samples_from_cdf(model_outputs, group_by_cols, n_samples)
+    model_outputs <- get_samples_from_cdf(model_outputs, task_id_cols, n_samples)
   } else if (starting_output_type == "quantile") {
     # if median output desired, and Q50 provided return exact value
     if (new_output_type == "median" && 0.5 %in% starting_output_type_ids) {
@@ -77,12 +74,12 @@ convert_output_type <- function(model_outputs, group_by_cols,
       return(model_outputs_transform)
     } else {
       # otherwise, estimate from samples
-      model_outputs <- get_samples_from_quantiles(model_outputs, group_by_cols, n_samples)
+      model_outputs <- get_samples_from_quantiles(model_outputs, task_id_cols, n_samples)
     }
   }
   # transform based on new_output_type
   grouped_model_outputs <- model_outputs %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(group_by_cols)))
+    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols)))
   model_outputs_transform <- convert_from_sample(
     grouped_model_outputs, new_output_type, new_output_type_id
   )
@@ -127,7 +124,7 @@ validate_new_output_type <- function(starting_output_type, new_output_type,
                 of the predictive distribution"
       ))
     }
-    if (any(new_output_type_id < 0 || new_output_type > 1)) {
+    if (any(new_output_type_id < 0) || any(new_output_type_id > 1)) {
       cli::cli_abort(c(
         "elements of {.var new_output_type_id} should be between 0 and 1",
         i = "elements of {.var new_output_type_id} represent quantiles
@@ -146,10 +143,10 @@ validate_new_output_type <- function(starting_output_type, new_output_type,
 }
 
 #' @export
-get_samples_from_quantiles <- function(model_outputs, group_by_cols, n_samples, ...) {
+get_samples_from_quantiles <- function(model_outputs, task_id_cols, n_samples, ...) {
   set.seed(101)
   samples <- model_outputs %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(group_by_cols))) %>%
+    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols))) %>%
     dplyr::reframe(
       value = distfromq::make_q_fn(
         ps = as.numeric(.data$output_type_id),
@@ -162,10 +159,10 @@ get_samples_from_quantiles <- function(model_outputs, group_by_cols, n_samples, 
 }
 
 #' @export
-get_samples_from_cdf <- function(model_outputs, group_by_cols, n_samples, ...) {
+get_samples_from_cdf <- function(model_outputs, task_id_cols, n_samples, ...) {
   set.seed(101)
   samples <- model_outputs %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(group_by_cols))) %>%
+    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols))) %>%
     dplyr::reframe(
       value = distfromq::make_q_fn(
         ps = .data$value,
