0f2061582d0181caf9df6e4e65652cb9c72c0188
Zhian N. Kamvar <zkamvar@gmail.com>
2024-07-31 14:07:24 -0700

    avoid unused imports note

--- R/zzz.R
+++ R/zzz.R
@@ -4,3 +4,13 @@
   get_schema <<- memoise::memoise(get_schema)
   get_schema_valid_versions <<- memoise::memoise(get_schema_valid_versions)
 }
+
+ignore_unused_imports <- function() {
+  # getting a note that R CMD check cannot detect unused imports
+  # Zhian suspects this is due to the memoi[sz]ation above and is using
+  # the Hadley hack: 
+  #  <https://github.com/hadley/r-pkgs/issues/828#issuecomment-1210558705>
+  fs::path
+  gh::gh
+  curl::curl_echo
+}
