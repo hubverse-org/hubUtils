944404d6c2e42d5e687dfda52d9e841eb0b7ea42
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-09-11 15:21:44 -0400

    Fix linting issues

--- R/convert_output_types.R
+++ R/convert_output_types.R
@@ -1,5 +1,5 @@
 #' Transform between output types
-#' 
+#'
 #' Transform between output types, from one starting output_type into new
 #' output_types. See details for supported conversions.
 #'
@@ -68,6 +68,7 @@
 #'
 #' @return object of class `model_out_tbl` containing new output_type
 #' @export
+#' @importFrom rlang .data
 convert_output_type <- function(model_out_tbl, new_output_type,
                                 new_output_type_id = NA, n_samples = 1e4, ...) {
   # validations
@@ -92,13 +93,13 @@ convert_output_type <- function(model_out_tbl, new_output_type,
   }
   # transform based on new_output_type
   grouped_model_out_tbl <- model_out_tbl %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols)))
+    dplyr::group_by("model_id", dplyr::across(dplyr::all_of(task_id_cols)))
   model_out_tbl_transform <- vector("list", length = length(new_output_type))
-  for (i in 1:length(new_output_type)) {
+  for (i in seq_along(new_output_type)) {
     # if median output desired, and Q50 provided return exact value
     if (new_output_type[i] == "median" && 0.5 %in% starting_output_type_ids) {
       model_out_tbl_transform[[i]] <- model_out_tbl %>%
-        dplyr::filter(output_type_id == 0.5) %>%
+        dplyr::filter(.data[["output_type_id"]] == 0.5) %>%
         dplyr::mutate(
           output_type = new_output_type[i],
           output_type_id = NA
@@ -189,11 +190,11 @@ validate_new_output_type <- function(starting_output_type, new_output_type,
 get_samples_from_quantiles <- function(model_out_tbl, task_id_cols, n_samples, ...) {
   set.seed(101)
   samples <- model_out_tbl %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols))) %>%
+    dplyr::group_by("model_id", dplyr::across(dplyr::all_of(task_id_cols))) %>%
     dplyr::reframe(
       value = distfromq::make_q_fn(
-        ps = as.numeric(.data$output_type_id),
-        qs = .data$value, ...
+        ps = as.numeric(.data[["output_type_id"]]),
+        qs = .data[["value"]], ...
       )(runif(n_samples, 0, 1))
     )
 
@@ -205,11 +206,11 @@ get_samples_from_quantiles <- function(model_out_tbl, task_id_cols, n_samples, .
 get_samples_from_cdf <- function(model_out_tbl, task_id_cols, n_samples, ...) {
   set.seed(101)
   samples <- model_out_tbl %>%
-    dplyr::group_by(model_id, dplyr::across(dplyr::all_of(task_id_cols))) %>%
+    dplyr::group_by("model_id", dplyr::across(dplyr::all_of(task_id_cols))) %>%
     dplyr::reframe(
       value = distfromq::make_q_fn(
-        ps = .data$value,
-        qs = as.numeric(.data$output_type_id), ...
+        ps = .data[["value"]],
+        qs = as.numeric(.data[["output_type_id"]]), ...
       )(runif(n_samples, 0, 1))
     )
   return(samples)
@@ -221,26 +222,25 @@ convert_from_sample <- function(grouped_model_out_tbl, new_output_type,
   if (new_output_type == "mean") {
     model_out_tbl_transform <- grouped_model_out_tbl %>%
       dplyr::reframe(
-        value = mean(value),
+        value = mean(.data[["value"]]),
         output_type_id = new_output_type_id
       )
   } else if (new_output_type == "median") {
     model_out_tbl_transform <- grouped_model_out_tbl %>%
       dplyr::reframe(
-        value = median(value),
+        value = mean(.data[["value"]]),
         output_type_id = new_output_type_id
       )
   } else if (new_output_type == "quantile") {
     model_out_tbl_transform <- grouped_model_out_tbl %>%
       dplyr::reframe(
-        value = quantile(value, as.numeric(new_output_type_id), names = FALSE
-        ),
+        value = quantile(.data[["value"]], as.numeric(new_output_type_id), names = FALSE),
         output_type_id = new_output_type_id
       )
   } else if (new_output_type == "cdf") {
     model_out_tbl_transform <- grouped_model_out_tbl %>%
       dplyr::reframe(
-        value = ecdf(value)(as.numeric(new_output_type_id)),
+        value = ecdf(.data[["value"]])(as.numeric(new_output_type_id)),
         output_type_id = new_output_type_id
       )
   }
