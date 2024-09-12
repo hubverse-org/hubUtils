db08c45eb4eac621de1bfe7da51572686eeef181
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-09-12 11:38:12 -0400

    Document `get_task_id_cols()`

--- R/get_task_id_cols.R
+++ R/get_task_id_cols.R
@@ -1,3 +1,8 @@
+#' Get task ID column names from `model_out_tbl` object
+#'
+#' @param model_out_tbl an object of class `model_out_tbl` 
+#'
+#' @return a character vector of task ID column names
 #' @export
 get_task_id_cols <- function(model_out_tbl) {
   model_out_cols <- colnames(model_out_tbl)
