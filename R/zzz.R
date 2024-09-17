94d9e8074467acd02061e152ea03bb1aef1c948f
Anna Krystalli <annakrystalli@googlemail.com>
2024-09-17 19:03:19 +0300

    Add comment regarding usage of <<-

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
+# We use `<<-` below to modify the package's namespace.
+# It doesn't modify the global environment.
+# We do this to prevent build time dependencies on {memoise},
+# as recommended in <http://memoise.r-lib.org/reference/memoise.html#details>.
+# Cf. <https://github.com/r-lib/memoise/issues/76> for further details.
 .onLoad <- function(libname, pkgname) {
   read_config.default <<- memoise::memoise(read_config.default)
   read_config.SubTreeFileSystem <<- memoise::memoise(read_config.SubTreeFileSystem)
