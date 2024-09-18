e55522f05fbe50bc2de6385bb21dff1469ee34b5
Anna Krystalli <annakrystalli@googlemail.com>
2024-09-18 13:35:32 +0300

    Fix lintr

--- data-raw/hub_con_output.R
+++ data-raw/hub_con_output.R
@@ -2,9 +2,9 @@
 hub_path <- system.file("testhubs/flusight", package = "hubUtils")
 hub_con <- hubData::connect_hub(hub_path)
 hub_con_output <- hub_con %>%
-    dplyr::filter(output_type == "quantile", location == "US") %>%
-    dplyr::collect() %>%
-    dplyr::filter(forecast_date == max(forecast_date))
+  dplyr::filter(output_type == "quantile", location == "US") %>%
+  dplyr::collect() %>%
+  dplyr::filter(forecast_date == max(forecast_date))
 
 
 usethis::use_data(hub_con_output, overwrite = TRUE)
